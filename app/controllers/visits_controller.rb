9b224b580d2716ff3f97a27241bef27412b3b263
Peter Strong <peter.strong@gmail.com>
2017-02-16 18:03:59 -0500

    fix gsub for http

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -5,7 +5,7 @@ class VisitsController < ApplicationController
     
     if @link 
       Visit.create(link_id: @link.id, ip_address: request.remote_ip)
-      redirect_to "#{@link.target_url}"
+      redirect_to "https://#{@link.target_url}"
     else
       render :file => "#{Rails.root}/public/404.html", :status => 404
     end  
